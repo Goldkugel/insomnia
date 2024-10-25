e36da5ba1c7fa353962069cbabbb0b1fdb55ee36
Goldkugel <peter.pallaoro@tum.de>
2024-10-25 16:03:34 +0200

    Finalized and tested the function addSegments.

--- src/addSegments.R
+++ src/addSegments.R
@@ -21,8 +21,63 @@ addSegment <- function
     time_column = "t",
     id_column = "ICUSTAY_ID",
     segment_length = THREE * SEC_PER_DAY,
-    segment_offset = SEC_PER_DAY
+    segment_offset = SEC_PER_DAY,
+    segment_column = "segment_id",
+    tmp = "./.tmp.csv"
 ) 
 {
+  result <- data.frame(data)
+  result[segment_column] <- rep(-ONE, nrow(data))
+  result <- data.frame(result)[FALSE,]
   
+  write.table(
+    result, 
+    tmp, 
+    sep = ";", 
+    row.names = FALSE, 
+    col.names = TRUE, 
+    append = FALSE
+  )
+  
+  ids <- unique(data[id_column])
+  row.names(ids) <- NULL
+  
+  count <- ZERO
+  for (id in ids[, id_column]) {
+    count <- count + ONE
+    
+    # Print the current progress.
+    print(count / length(ids[, id_column]))
+    rows <- data[data[, id_column] == id, ]
+    
+    max_time <- max(rows[time_column])
+    min_time <- ZERO
+    
+    segment_count <- max(c(ONE, 
+       ceiling((max_time - segment_length) / segment_offset))
+    )
+    
+    for (segment_id in ONE:segment_count) {
+      segment_rows <- rows[
+        rows[time_column] >= segment_offset * (segment_id - ONE) & 
+        rows[time_column] <  segment_offset * segment_id + segment_length
+      ,]
+      
+      if(nrow(segment_rows[]) > ZERO) {
+        segment_rows[segment_column] <- segment_id
+        row.names(result) <- NULL
+        
+        write.table(
+          segment_rows, 
+          file = tmp, 
+          append = TRUE, 
+          col.names = FALSE, 
+          sep = ";", 
+          row.names = FALSE
+        ) 
+      }
+    }
+  }
+
+  return(read.table(file = tmp, sep = ";", row.names = NULL))
 }
